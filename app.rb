7ca22822a5684ea8c2232c57e75f674864897307
Satoshi Ebisawa <e.satoshi@gmail.com>
2011-02-10 00:40:52 +0900

    some modifications

--- app.rb
+++ app.rb
@@ -1,3 +1,3 @@
 get '/' do
-  'Hello, world!'
+  redirect '/index.html'
 end
--- websocket.rb
+++ websocket.rb
@@ -1,18 +1,23 @@
 require 'em-websocket'
+require 'logger'
 
 EventMachine.run {
+  @logger = Logger.new(STDERR)
   @channel = EM::Channel.new
   
   EventMachine::WebSocket.start(:host => '0.0.0.0', :port => 8080) do |ws|
     ws.onopen {
       sid = @channel.subscribe{|msg| ws.send msg}
+      @logger.debug "#{sid} connected!"
       @channel.push "#{sid} connected!"
       
       ws.onmessage {|msg|
+        @logger.debug "<#{sid}>: #{msg}"
         @channel.push "<#{sid}>: #{msg}"
       }
       ws.onclose {
         @channel.unsubscribe(sid)
+        @logger.debug "#{sid} disconnected!"
         @channel.push "#{sid} disconnected!"
       }
     }
