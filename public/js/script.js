733938914696ba557d9b59b433c8e385ce53a3d9
Satoshi Ebisawa <e.satoshi@gmail.com>
2011-02-11 00:28:09 +0900

    refactor

--- public/js/script.js
+++ public/js/script.js
@@ -1,65 +1,96 @@
-var host = location.hostname;
-var url = 'ws://'+host+':8080/';
-var ws = new WebSocket(url);
 (function($){
+  var host = location.hostname;
+  var url = 'ws://'+host+':8080/';
+  var ws = new WebSocket(url);
+  var $ws = $(ws);
   $(function(){
-    var $ws = $(ws);
-
-    var dispMessage = function(msg) {
-      var $elem = $('<li/>').text(msg);
-      $elem.prependTo($('#log'));
-    };
-
     $ws.bind('open', function(){
-      $('#send-button').click(function(){
-        var message = $('#message').val();
-        if (message == '') return;
-        ws.send(message);
-        $('#message').val('');
-      });
-      dispMessage('connected');
     });
-
     $ws.bind('close', function(){
-      $('#send-button').unbind('click');
-      dispMessage('disconnected');
     });
-
-    // $ws.bind('message', function(event){
-      // var msg = event.originalEvent.data;
-      // if (msg == '') return;
-      // dispMessage("> "+msg);
-    // });
-
     $(window).unload(function(){
       ws.close();
     });
+  });
 
-    $(window).bind('devicemotion', function(event){
-      var threshold = 5;
-      // var acc = event.originalEvent.accelerationIncludingGravity;
-      var acc = event.originalEvent.acceleration;
-      if (Math.abs(acc.x) > threshold) {
-        if (acc.x > 0) {
-          ws.send('右');
-        } else {
-          ws.send('左');
-        }
+  // load google map
+  $('#page-map').live('pagecreate', function(){
+    // load GMap
+    var mapOptions = {
+      zoom: 12,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDoubleClickZoom: true
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var initialLocation;
+    navigator.geolocation.getCurrentPosition(function(position){
+      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      map.setCenter(initialLocation);
+    });
+
+    var labelOpt = {
+      content: '',
+      boxStyle: {
+        border: "1px solid black",
+        textAlign: "center",
+        fontSize: "8pt",
+        width: "100px",
+        backgroundColor: 'yellow'
+      },
+      disableAutoPan: true,
+      pixelOffset: new google.maps.Size(-50, 0),
+      position: initialLocation,
+      closeBoxURL: "",
+      isHidden: false,
+      pane: "mapPane",
+      enableEventPropagation: true
+    };
+
+    google.maps.event.addListener(map, 'dblclick', function(event){
+      $.mobile.changePage($('#page-message'), 'pop', false, false);
+
+      var marker = new google.maps.Marker({
+        position: event.latLng,
+        map: map,
+        title: '',
+        draggable: true
+      });
+
+      var ibLabel = new InfoBox(labelOpt);
+
+      $('#set-message').unbind();
+      $('#set-message').bind('click', function(){
+        marker.setTitle($('#message').val());
+        ibLabel.setContent($('#message').val());
+        ibLabel.open(map);
+        $('#message').val('');
+        $.mobile.changePage($('#page-map'), 'pop', true, false);
+        ws.send(marker.getTitle()+':'+marker.getPosition().toString());
+      });
+
+      google.maps.event.addListener(marker, 'click', function(){
+        ibLabel.open(map);
+      });
+    }); // map.doubleclick
+
+    $(ws).bind('message', function(event){
+      var msg = event.originalEvent.data;
+      if (!msg.match(/(.+):\((.+), (.+)\)/)) {
+        return;
       }
-      if (Math.abs(acc.y) > threshold) {
-        if (acc.y > 0) {
-          ws.send('上');
-        } else {
-          ws.send('下');
-        }
-      }
-      if (Math.abs(acc.z) > threshold) {
-        if (acc.z > 0) {
-          ws.send('手前');
-        } else {
-          ws.send('奥');
-        }
-      }
+      var title = RegExp.$1;
+      var lat = RegExp.$2;
+      var lng = RegExp.$3;
+      var latlng = new google.maps.LatLng(lat, lng);
+      var marker = new google.maps.Marker({
+        title: title,
+        position: latlng,
+        map: map
+      });
+      var ibLabel = new InfoBox(labelOpt);
+      ibLabel.setContent(title);
+      ibLabel.setPosition(latlng);
+      ibLabel.open(map);
     });
   });
 
--- websocket.rb
+++ websocket.rb
@@ -9,7 +9,6 @@ EventMachine.run {
     ws.onopen {
       sid = @channel.subscribe{|msg| ws.send msg}
       @logger.debug "#{sid} connected!"
-      # @channel.push "#{sid} connected!"
       
       ws.onmessage {|msg|
         @logger.debug "<#{sid}>: #{msg}"
@@ -18,7 +17,6 @@ EventMachine.run {
       ws.onclose {
         @channel.unsubscribe(sid)
         @logger.debug "#{sid} disconnected!"
-        # @channel.push "#{sid} disconnected!"
       }
     }
   end
